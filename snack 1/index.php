8074d59455daa60f9dac191ab48d58d7d2c7efc0
Eddishot <Eddi_sl94@hotmail.it>
2021-11-29 17:31:56 +0100

    first commit

---
+++ snack 1/index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Snack 1
+Passare come parametri GET name, mail e age e verificare
+ (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri
+, che mail contenga un punto e una chiocciola e che age sia un numero.
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+*/
+
+$nameGet = $_GET["name"];
+$emailGet = $_GET["email"];
+$ageGet = $_GET["age"];
+
+$hasDot = strchr($emailGet, ".");
+$hasAt = strchr($emailGet, "@");
+
+
+
+if(strlen($nameGet) > 3){
+    echo "accesso riuscito";
+} else{
+    echo "accesso rifiutato";
+}
+
+if($hasDot && $hasAt){
+    echo "OOKK";
+}else{
+ echo"NOT OOK";
+}
+
+?>
