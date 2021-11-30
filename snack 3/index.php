b952dcbfd1a11aa8187b65e4905fa668c6bf7254
Eddishot <Eddi_sl94@hotmail.it>
2021-11-30 11:03:27 +0100

    exercise 3 completed

--- snack 3/index.php
+++ snack 3/index.php
@@ -3,4 +3,19 @@
 Snack 3
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
 */
+
+
+
+$numList = [];
+$numTotali = 15;
+
+while(count($numList) < $numtotali){
+    $numRandom = rand(1, 100);
+
+// controllo se dentro l´array dei numeri ci sono i numeri random, se no li aggiungo dentro l´array
+    if(!in_array($numRandom, $numList)){
+
+        $numList[] = $numRandom;
+    }
+}
 ?>
